5e16aef422608cef3758a73d4f82fcead5003aed
G Codes <graciela.mejia1@yahoo.com>
2022-10-26 18:17:19 -0700

    done with 1st lesson

---
+++ models/User.js
@@ -0,0 +1,67 @@
+//my note:import class types objects from Sequelize
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//create our User model
+class User extends Model {}
+//my note: extends keyword allows User to inherit all of the functionality of the functionality the `model` class has
+
+//define table columns and configuration
+User.init(
+  //my note:init method initializes the model's data+config, and passing 2 object arguements in it
+  {
+    //table column definitions go here (my note:this is the first object arguement)
+    //define an id column
+    id: {
+      //use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      //this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      //instruct that this is the Primary Key
+      primaryKey: true,
+      //turn on auto increment
+      autoIncrement: true,
+    },
+    //define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //define an email colume
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //there cannot be any duplicate email values in this table
+      unique: true,
+      //if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true,
+      },
+    },
+    //define a password colum
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        //this means the password must be at least four characters long
+        len: [4],
+      },
+    },
+  },
+  {
+    //table config options go here (https://sequelize.org/v5/manual/models-definition.html#configuration)
+
+    //pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    //don't automatically create createAt/updatedAt timestamp fields
+    timestamps: false,
+    //don't pluralize name of database table
+    freezeTableName: true,
+    //use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    //make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,25 @@
+//import express
+const express = require("express");
+//import routes
+const routes = require("./routes");
+//import sequelize
+const sequelize = require("./config/connection");
+
+//set up port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//turn on routes
+//my note: because of the way routes were set up in routes/index.js we don't need to import a bunch of different routes, everything is collected and packaged from that one file and imported here
+app.use(routes);
+
+//turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
+//my note: importing Sequelize at the top of file from config/connection.js then here use `sequelize.sync()` method to establish the connection to the database. The `sync` part means that this is Sequelize taking the models and connecting them to associated database tables, if it doesn't find a table, it'll create one.
+//nootice 'force:false', if force were set to true, it will drop and re-create all databases tables on start-up.
