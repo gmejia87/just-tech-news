8ad90aa4bde117a74e5325c642e3f46d23d7ca99
G Codes <graciela.mejia1@yahoo.com>
2022-10-26 22:50:26 -0700

    add Post to model/update routes/index.js to import Post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 //import User model
 const User = require("./User");
 
+//import Post model
+const Post = require("./Post");
+
 //export models
-module.exports = { User };
+module.exports = { User, Post };
