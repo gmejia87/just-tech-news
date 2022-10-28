cb4a4bebedde3522db1cefaff5ddf38fca7a7fb9
G Codes <graciela.mejia1@yahoo.com>
2022-10-27 22:40:46 -0700

    add vote routes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@
 const User = require("./User");
 //import Post model
 const Post = require("./Post");
+//import Vote model
+const Vote = require("./Vote");
 
 // create associations
 User.hasMany(Post, {
@@ -12,5 +14,33 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "psot_id",
+});
+
 //export models
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
